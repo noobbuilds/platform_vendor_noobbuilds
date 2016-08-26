cdd802ddb50f9962fc662b4d4d174f2ff1107e0c
beanstown106 <nbenis106@gmail.com>
2016-08-26 18:45:58 -0400

    aosp_fixes: add ro.atrace.core.services prop

--- configs/common.mk
+++ configs/common.mk
@@ -12,9 +12,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.com.android.dateformat=MM-dd-yyyy \
     drm.service.enabled=true \
     ro.build.selinux=1 
+
      
 # Common overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/noobbuilds/overlays/common
