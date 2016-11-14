e701e0115080104d96b6c6f1944f55c943c7d40f
fwhem <fwhemingway@gmail.com>
2016-11-14 10:18:27 -0500

    Add noob common make.

---
+++ configs/common.mk
@@ -0,0 +1,33 @@
+# Noobbuilds common make file
+
+
+# Local path for prebuilts
+LOCAL_PATH:= vendor/noobbuilds/prebuilt
+
+# Common build prop overrides 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    drm.service.enabled=true \
+    ro.build.selinux=1 
+     
+# Common overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/noobbuilds/overlays/common
+
+# Added Packages
+PRODUCT_PACKAGES += \
+    Launcher3  
+    
+# Add noobbuilds sounds
+-include vendor/noobbuilds/configs/noob_sounds.mk
+    
+# Add stock bootanimation 
+-include vendor/noobbuilds/configs/bootani.mk
+
+# Add fingerprints
+-include vendor/noobbuilds/configs/noob_fingerprints.mk
