290d2d19501a4a09d8ffb465f716ed72f560d278
fwhem <fwhemingway@gmail.com>
2017-01-04 08:01:45 -0500

    Update fingerprints for Jan '17 patches.

--- configs/noob_fingerprints.mk
+++ configs/noob_fingerprints.mk
@@ -3,15 +3,15 @@
 ifeq ($(TARGET_PRODUCT),angler)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),flounder)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=volantis \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/NMF26F/3425388:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 7.1.1 NMF26F 3425388 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26M/3562722:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26M 3562722 release-keys"
 endif
 
        
