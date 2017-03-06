af98dc3b575602b79c3ac208559b2e398d245e37
fwhem <fwhemingway@gmail.com>
2017-03-06 18:35:41 -0500

    Update fingerprints for Mar security patches.

--- configs/noob_fingerprints.mk
+++ configs/noob_fingerprints.mk
@@ -3,22 +3,22 @@
 ifeq ($(TARGET_PRODUCT),angler)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26O/3582057:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26O 3582057 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26T/3687331:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26T 3687331 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),flounder)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=volantis \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26Q/3623443:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26Q 3623443 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26T/3687331:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26T 3687331 release-keys"
 endif
 
 ifeq ($(TARGET_PRODUCT),shamu)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F26R/3582520:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F26R 3582520 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F26U/3687496:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F26U 3687496 release-keys"
 endif
 
        
