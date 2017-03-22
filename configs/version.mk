ed7276c7be29911e72e99ba3729bf6cefeaffb78
fwhem <fwhemingway@gmail.com>
2017-03-22 07:50:11 -0400

    Edit build.prop for subs recognition.

--- configs/version.mk
+++ configs/version.mk
@@ -1,7 +1,9 @@
 # Noobbuilds version make
 
-ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d-%H%M)
 ROM_NAME := noobbuilds
 
+ROM_VERSION := $(ROM_NAME)-$(PLATFORM_VERSION)-$(shell date +%Y%m%d-%H%M)
+
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rom.version=$(ROM_VERSION)
+    ro.noob.version=$(ROM_VERSION)
