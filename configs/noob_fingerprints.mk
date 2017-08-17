8509c35dcedb68df3560ff6acc08ddef8f3a6382
fwhem <fwhemingway@gmail.com>
2017-08-17 08:44:48 -0400

    Change Shamu Aug fingerprint.

--- configs/noob_fingerprints.mk
+++ configs/noob_fingerprints.mk
@@ -17,8 +17,8 @@ endif
 ifeq ($(TARGET_PRODUCT),shamu)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F27I/4118426:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27I 4118426 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N8I11B/4171878:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N8I11B 4171878 release-keys"
 endif
 
        
