d33dfa8bc7a48995a10b6a6b026234aa1841eb09
fwhem <fwhemingway@gmail.com>
2017-09-06 18:15:53 -0400

    Wishing and hoping.

---
+++ products/flounder.mk
@@ -0,0 +1,15 @@
+# Noobbuilds flounder make file
+
+# Inlude tablet make file
+include vendor/noobbuilds/configs/noob_tab.mk
+
+# Inherit AOSP device configuration for mako
+$(call inherit-product, device/htc/flounder/aosp_flounder.mk)
+
+
+# Override AOSP build properties
+PRODUCT_NAME := flounder
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := flounder
+PRODUCT_MODEL := Nexus 9
+PRODUCT_MANUFACTURER := HTC
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
 # Noobbuilds lunch
 add_lunch_combo angler-userdebug
+add_lunch_combo flounder-userdebug
+add_lunch_combo shamu-userdebug
