5de9e0c4893e7d8569dcf76007b988f861d7c6e3
fwhem <fwhemingway@gmail.com>
2017-09-09 14:08:53 -0400

    Add bootani.mk and black Google Pixel bootani's.

---
+++ configs/bootani.mk
@@ -0,0 +1,17 @@
+# Include stock N bootanimation.
+
+ifeq (angler,$(TARGET_PRODUCT))
+PRODUCT_COPY_FILES += \
+    vendor/noobbuilds/prebuilt/media/angler.zip:system/media/bootanimation.zip
+endif
+
+ifeq (flounder,$(TARGET_PRODUCT))
+PRODUCT_COPY_FILES += \
+    vendor/noobbuilds/prebuilt/media/flounder.zip:system/media/bootanimation.zip 
+endif
+
+ifeq (shamu,$(TARGET_PRODUCT))
+PRODUCT_COPY_FILES += \
+    vendor/noobbuilds/prebuilt/media/shamu.zip:system/media/bootanimation.zip
+endif
+
